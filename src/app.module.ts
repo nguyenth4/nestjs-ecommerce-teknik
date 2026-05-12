753427f1a60a40d3eda1102301578af7cf62dbd2
Nguyen284 <nguyenhoang280004@gmail.com>
2026-05-12 11:02:48 +0700

    feat: Implement Week 2 - Database, Authentication & RBAC

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule, 
+    UsersModule, 
+    AuthModule, 
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
